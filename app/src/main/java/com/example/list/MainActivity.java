22c7061d5cbd7dfea9ac851a3d961a7a2917a97c
Unicorn <svyatoslav.raklov@bk.ru>
2020-10-09 10:38:54 +0300

    recyclerview

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private ListAdapter adapter;
+    private List<Person> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        Person person = new Person();
+        person.setName("Ivan ivanov");
+        person.setPhone("+79246547821");
+        person.setEmail("ivan@mail.ru");
+        list.add(person);
+
+        adapter = new ListAdapter(this, list);
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
